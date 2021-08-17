6518a55a056dfe55a7df5a7fe3eb98cd6a9b5558
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-17 18:07:41 -0300

    :wrench: feat: created routes

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import { Route, Switch } from 'react-router-dom';
+import Home from '../pages/Home';
+
+export function Routes() {
+    return (
+        <>
+            <Switch>
+                <Route path="/" exact component={Home} />
+            </Switch>
+        </>
+    );
+}
