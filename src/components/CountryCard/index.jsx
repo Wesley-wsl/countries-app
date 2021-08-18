0f8cf5218cf081d83a815ef9a90c0d9529d87f74
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-18 20:24:52 -0300

    :wrench: feat: Added link to page details.

--- src/components/CountryCard/index.jsx
+++ src/components/CountryCard/index.jsx
@@ -1,18 +1,23 @@
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
 import { CardStyle } from './styles';
 
-export default function CountryCard() {
+export default function CountryCard(props) {
     return (
         <CardStyle>
-            <img src="https://restcountries.eu/data/afg.svg" alt="Flag" />
-            <h3>Example</h3>
+            <Link to={`details/${props.data.alpha3Code}`}>
+                <img src={props.data.flag} alt={`Flag ${props.data.name}`} />
+                <h3>{props.data.name}</h3>
+            </Link>
             <p>
-                <span>Population</span>: 81,770,900
+                <span>Population</span>:{' '}
+                {props.data.population.toLocaleString()}
             </p>
             <p>
-                <span>Region</span>: Europe
+                <span>Region</span>: {props.data.region}
             </p>
             <p>
-                <span>Capital</span>: Berlin
+                <span>Capital</span>: {props.data.capital}
             </p>
         </CardStyle>
     );
