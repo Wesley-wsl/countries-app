7a998d9bb9ace8b0c42dcfcb47c8fd393a26d546
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-18 20:16:12 -0300

    :wrench: feat: added new route for country details

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,17 @@
 import { Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
+import CountryDetails from '../pages/CountryDetails';
 
 export function Routes() {
     return (
         <>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route
+                    path="/details/:alphaCode"
+                    exact
+                    component={CountryDetails}
+                />
             </Switch>
         </>
     );
