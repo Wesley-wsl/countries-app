16126e465147c2326d239453e3e01263eff7d1e7
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-19 19:13:09 -0300

    :art: style: created configs themes dark/light

---
+++ src/styles/themes/index.js
@@ -0,0 +1,13 @@
+export const Dark = {
+    body: 'hsl(207, 26%, 17%)',
+    elements: 'hsl(209, 23%, 22%)',
+    color: '#fff',
+    boxShadow: 'hsl(0, 0%, 12%)',
+};
+
+export const Light = {
+    body: 'hsl(0, 0%, 95%)',
+    elements: 'hsl(0, 0%, 100%)',
+    color: '#000',
+    boxShadow: 'hsl(0, 0%, 82%)',
+};
