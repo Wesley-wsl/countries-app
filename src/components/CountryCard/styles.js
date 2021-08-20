3784c5d3ed35ce473d107e540b4f3dd4cb5ed1c3
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-20 16:35:32 -0300

    :art: style: Added hover effect

--- src/components/CountryCard/styles.js
+++ src/components/CountryCard/styles.js
@@ -2,10 +2,17 @@ import styled from 'styled-components';
 
 export const CardStyle = styled.div`
     background-color: ${props => props.theme.elements};
-    border-radius: 10px;
-    height: 310px;
+    border-radius: 7px;
+    height: 320px;
     margin-bottom: 40px;
     margin-right: 45px;
+    box-shadow: 0px 0px 3px ${props => props.theme.boxShadow};
+    transition: transform 0.2s linear;
+
+    &:hover {
+        transform: scale(105%);
+    }
+
     img {
         width: 250px;
         border-top-right-radius: 7px;
@@ -14,18 +21,26 @@ export const CardStyle = styled.div`
     }
 
     h3 {
-        margin: 10px 0px;
-        max-width: 250px;
+        margin-top: 20px;
         overflow-x: auto;
     }
 
     h3,
     p {
-        margin-left: 10px;
+        max-width: 220px;
+        margin: 2px auto;
     }
 
-    p > span {
-        font-weight: 600;
+    p {
+        font-size: 14px;
+        padding: 2px;
+        span {
+            font-weight: 600;
+        }
+    }
+
+    p:nth-last-child(1) {
+        height: 70px;
     }
 
     @media (max-width: 480px) {
