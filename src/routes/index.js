9a8a09db971db98e46636d07e5c1bf818ebef716
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-20 16:34:48 -0300

    :wrench: feat: added new router (NotFound)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import CountryDetails from '../pages/CountryDetails';
+import { NotFound } from '../pages/NotFound';
 
 export function Routes() {
     return (
@@ -12,6 +13,7 @@ export function Routes() {
                     exact
                     component={CountryDetails}
                 />
+                <Route pat="*" component={NotFound} />
             </Switch>
         </>
     );
