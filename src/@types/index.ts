3d00ddb398a167d8af103db0e6bdfb9e962533d2
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-21 14:53:24 -0300

    :pencil2: chore: add types

---
+++ src/@types/index.ts
@@ -0,0 +1,40 @@
+export interface ICountries {
+    name: string;
+    alpha3Code: string;
+    population: number;
+    region: string;
+    capital: string;
+    flag: string;
+    numericCode: string;
+    nativeName: string;
+    subregion: string;
+    topLevelDomain: string[];
+    currencies: ICurrencies[];
+    languages: ILanguages[];
+    borders: string[];
+}
+
+export interface ICountriesData {
+    data: ICountries;
+}
+
+export interface ICurrencies {
+    code: string;
+    name: string;
+}
+
+export interface ILanguages {
+    name: string;
+    nativeName: string;
+}
+
+export interface ITheme {
+    theme: IThemeProps;
+}
+
+export interface IThemeProps {
+    body: string;
+    elements: string;
+    color: string;
+    boxShadow: string;
+}
