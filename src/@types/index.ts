0c23389d13cebe6a81e6375fc64a141a16c60f0b
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-22 00:01:58 -0300

    :pencil2: chore: add types to Header and Filter component

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,3 +1,4 @@
+import { Dispatch, SetStateAction } from 'react';
 export interface ICountries {
     name: string;
     alpha3Code: string;
@@ -13,28 +14,29 @@ export interface ICountries {
     languages: ILanguages[];
     borders: string[];
 }
-
 export interface ICountriesData {
     data: ICountries;
 }
-
 export interface ICurrencies {
     code: string;
     name: string;
 }
-
 export interface ILanguages {
     name: string;
     nativeName: string;
 }
-
 export interface ITheme {
     theme: IThemeProps;
 }
-
 export interface IThemeProps {
     body: string;
     elements: string;
     color: string;
     boxShadow: string;
+}
+export interface IHeader {
+    themeToggler: () => void;
+}
+export interface IFilter {
+    setCountry: Dispatch<SetStateAction<ICountries[]>>;
 }
