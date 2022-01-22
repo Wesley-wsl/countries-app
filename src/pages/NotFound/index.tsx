edbab550ca11a80363cbc8031bb50f05e1e7516d
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-22 00:10:39 -0300

    :recycle: refactor: move Header component to App.tsx

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,13 +1,9 @@
-import Header from '../../components/Header';
 import { ErrorStyle } from './styles';
 
 export function NotFound() {
     return (
-        <>
-            <Header />
-            <ErrorStyle>
-                <h2>Not Found</h2>
-            </ErrorStyle>
-        </>
+        <ErrorStyle>
+            <h2>Not Found</h2>
+        </ErrorStyle>
     );
 }
