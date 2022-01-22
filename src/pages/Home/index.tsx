f5a81fc01e6b38c7d1637375981f7e8a0d4a237d
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-22 00:08:00 -0300

    :recycle: refactor: use hooks to consume api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,85 +1,52 @@
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { ICountries } from '../../@types';
 import CountryCard from '../../components/CountryCard';
-import Header from '../../components/Header';
+import Filter from '../../components/Filter';
 import api from '../../services/api';
-import { SearchCountry, Countries } from './styles';
+import * as S from './styles';
 
 export default function Home() {
     const [country, setCountry] = useState<ICountries[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [selectValue, setSelectValue] = useState('all');
     const [searchValue, setSearchValue] = useState('');
+    const navigate = useNavigate();
 
     function Search(e: FormEvent) {
         e.preventDefault();
-        api.get(`name/${searchValue.trim()}`)
-            .then(res => setCountry(res.data))
-            .catch(error => console.log(error));
-    }
 
-    function FilterCountry() {
-        api.get(`region/${selectValue}`)
-            .then(res => setCountry(res.data))
-            .catch(error => console.log(error));
-    }
+        if (searchValue.trim() === '') return;
 
-    useEffect(() => {
-        api.get('all')
+        api.get(`name/${searchValue}`)
             .then(res => {
-                if (selectValue !== 'all') FilterCountry();
-                if (selectValue === 'all') setCountry(res.data);
-                setLoading(false);
+                if (res.data.status == 404) return navigate('/notFound');
+                setCountry(res.data);
             })
-            .catch(error => console.log(error));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [selectValue, searchValue]);
+            .catch(() => navigate('/notFound'));
+    }
 
     return (
-        <>
-            <Header />
-            <main>
-                <SearchCountry>
-                    <form className="search" onSubmit={Search}>
-                        <button type="submit">
-                            <i className="fas fa-search"></i>
-                        </button>
-                        <input
-                            type="text"
-                            placeholder="Search for a country..."
-                            value={searchValue}
-                            onChange={e => setSearchValue(e.target.value)}
-                        />
-                    </form>
-                    <div>
-                        <select
-                            name="regions"
-                            id="regions"
-                            value={selectValue}
-                            onChange={e => setSelectValue(e.target.value)}
-                        >
-                            <option value="all" style={{ display: 'none' }}>
-                                Filter By Regions
-                            </option>
-                            <option value="Africa">África</option>
-                            <option value="Americas">America</option>
-                            <option value="Asia">Asia</option>
-                            <option value="Europe">Europe</option>
-                            <option value="Oceania">Oceania</option>
-                        </select>
-                        <i className="fas fa-angle-down"></i>
-                    </div>
-                </SearchCountry>
+        <main>
+            <S.SearchCountry>
+                <S.Search onSubmit={Search}>
+                    <button type="submit" aria-label="Search country by name">
+                        <i className="fas fa-search"></i>
+                    </button>
+                    <input
+                        type="text"
+                        placeholder="Search for a country..."
+                        value={searchValue}
+                        onChange={e => setSearchValue(e.target.value)}
+                    />
+                </S.Search>
+                <Filter setCountry={setCountry} />
+            </S.SearchCountry>
 
-                <Countries>
-                    {loading == false
-                        ? country.map(data => (
-                              <CountryCard data={data} key={data.numericCode} />
-                          ))
-                        : 'Loading...'}
-                </Countries>
-            </main>
-        </>
+            <S.Countries>
+                {country.map(data => (
+                    <CountryCard data={data} key={data.numericCode} />
+                ))}
+            </S.Countries>
+        </main>
     );
 }
