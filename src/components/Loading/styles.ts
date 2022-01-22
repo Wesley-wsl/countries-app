8f529dfeea551e6cd67742f2332b375c36bd2cef
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-21 21:43:38 -0300

    :art: style: centralize loading

---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+import { ITheme } from '../../@types';
+
+export const Container = styled.div`
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: ${({ theme }: ITheme) => theme.body};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
