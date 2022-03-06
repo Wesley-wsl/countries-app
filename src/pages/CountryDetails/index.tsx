52e1697cdc225a094bdbe3626ebb0413e2a7b853
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-06 12:10:52 -0300

    :recycle: refactor: use swr

--- src/pages/CountryDetails/index.tsx
+++ src/pages/CountryDetails/index.tsx
@@ -1,15 +1,15 @@
 import { Link, useParams } from 'react-router-dom';
 
 import Loading from '../../components/Loading';
-import { useCountry } from '../../hooks/useCountry';
+import { useFetch } from '../../hooks/useFetch';
 import countryCodes from '../../utils/countryCodes';
 import * as S from './styles';
 
 export default function CountryDetails() {
     const { alphaCode } = useParams();
-    const { loading, country } = useCountry(alphaCode || '');
+    const { data } = useFetch(`alpha/${alphaCode}`);
 
-    if (loading) return <Loading />;
+    if (!data) return <Loading />;
 
     return (
         <main>
@@ -18,63 +18,70 @@ export default function CountryDetails() {
             </S.Back>
 
             <S.Details>
-                <img src={country.flag} alt={`Flag of ${country.name}`} />
+                <img src={data.flag} alt={`Flag of ${data.name}`} />
 
                 <div id="details">
-                    <h2>{country.name}</h2>
+                    <h2>{data.name}</h2>
                     <S.Informations>
                         <div className="left">
                             <p>
-                                <span>Native Name:</span> {country.nativeName}
+                                <span>Native Name:</span> {data.nativeName}
                             </p>
                             <p>
                                 <span>Population:</span>{' '}
-                                {country.population.toLocaleString()}
+                                {data.population.toLocaleString()}
                             </p>
                             <p>
-                                <span>Region:</span> {country.region}
+                                <span>Region:</span> {data.region}
                             </p>
                             <p>
-                                <span>Sub Region:</span> {country.subregion}
+                                <span>Sub Region:</span> {data.subregion}
                             </p>
                             <p>
-                                <span>Capital:</span> {country.capital}
+                                <span>Capital:</span> {data.capital}
                             </p>
                         </div>
 
                         <div>
                             <p>
                                 <span>Top Level Domain:</span>{' '}
-                                {country.topLevelDomain}
+                                {data.topLevelDomain}
                             </p>
                             <p>
                                 <span>Currencies:</span>{' '}
-                                {country.currencies[0].name}
+                                {data.currencies[0].name}
                             </p>
                             <p>
                                 <span>Languages:</span>{' '}
-                                {country.languages.map((language, index) => {
-                                    if (country.languages.length === index + 1)
-                                        return `${language.name}.`;
+                                {data.languages.map(
+                                    (
+                                        language: { name: string },
+                                        index: number,
+                                    ) => {
+                                        if (data.languages.length === index + 1)
+                                            return `${language.name}.`;
 
-                                    return `${language.name}, `;
-                                })}
+                                        return `${language.name}, `;
+                                    },
+                                )}
                             </p>
                         </div>
                     </S.Informations>
                     <S.BorderCountries>
                         <p>
                             <span>Border Countries:</span>{' '}
-                            {country.borders
-                                ? country.borders.map((border, index) => (
-                                      <Link
-                                          to={`/details/${border}`}
-                                          className="border"
-                                          key={index}
-                                      >
-                                          {countryCodes[border]}
-                                      </Link>
-                                  ))
+                            {data.borders
+                                ? data.borders.map(
+                                      (border: string, index: number) => (
+                                          <Link
+                                              to={`/details/${border}`}
+                                              className="border"
+                                              key={index}
+                                          >
+                                              {countryCodes[border]}
+                                          </Link>
+                                      ),
+                                  )
                                 : 'none'}
                         </p>
                     </S.BorderCountries>
